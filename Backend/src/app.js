41e705edb21038f10b1ba2f3c5f2f98c4f505592
Pawan kumar <pawankumarmalhi291@gmail.com>
2025-02-01 20:44:56 +0500

    updated code

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,7 +15,7 @@ const app = express();
 const server=createServer(app);
 const io=connectToSocket(server)
 
-app.set("port", (process.env.PORT || 8000))
+app.set("port", (process.env.PORT || 5000))
 app.use(cors())
 app.use(express.json({limit:"40kb", extended:true}))
 app.use(express.urlencoded({limit:"40kb" , extended:true}))
