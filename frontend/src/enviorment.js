3126bee26811e403a7f5f57e9f11100f7cf2e9d4
Pawan kumar <pawankumarmalhi291@gmail.com>
2025-02-22 15:18:52 +0500

    newUpdate

--- frontend/src/enviorment.js
+++ frontend/src/enviorment.js
@@ -1,5 +1,9 @@
-let IS_Pord= true;
+let IS_Pord = true;
 
-const server = IS_Pord ? "https://instant-meeting-backend.vercel.app": "http://localhost:8000";
+const server = IS_Pord
+  ? " https://instant-meeting-production.up.railway.app"
+  : "http://localhost:8000";
 
 export default server;
+
+// https://instant-meeting-backend.vercel.app
